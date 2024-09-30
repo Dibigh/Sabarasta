33ade00726d143255d3ef2186b7acc768d3f494f
Dibigh <dibighrai2017@gmail.com>
2024-09-30 19:12:53 +0545

    fully responsive navbar and footer with pic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import './Footer.css'
 import logo1 from '../../Logo/FooterLogo.svg'
 import icons from '../../Photos/Frame.png'
-import {ReactComponent as SVG} from '../../Logo/company show photo.svg' 
+import SvgPic from '../../Logo/companyShowPhoto.svg' 
 function Footer() {
   return (
     <div id='footer'>
       <div id = 'FooterPic'>
-        <SVG id='Pic' viewBox='0 0 1200 600'/>
+        <img id='Pic' src ={SvgPic}/>
       </div>
       <div id='SearchBar'>
         <p>Subscribe to <span>Our NewsLetter</span></p>
@@ -29,39 +29,37 @@ function Footer() {
         </div>
         <div id='footermenu'>
           <div id='services'>
-          <div><h3>Services</h3></div>
-            <div><p><Link to='/Services/IT-Consulting' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>IT Consulting</Link></p></div>
-            <div><p><Link to='/Services/WebDevelopment' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Web Design</Link></p></div>
-            <div><p><Link to='/Services/AppDevelopment' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>App Development</Link></p></div>
-            <div><p><Link to='/Services/UXUI' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>UX UI Design</Link></p></div>
-            <div><p>More...</p></div>
+            <h3>Services</h3>
+            <p><Link to='/Services/IT-Consulting' style={{textDecoration:'none',backgroundColor:'transparent',color:' rgba(255, 255, 255, 1)'}}>IT Consulting</Link></p>
+            <p><Link to='/Services/WebDevelopment' style={{textDecoration:'none',backgroundColor:'transparent',color:' rgba(255, 255, 255, 1)'}}>Web Design</Link></p>
+            <p><Link to='/Services/AppDevelopment' style={{textDecoration:'none',backgroundColor:'transparent',color:' rgba(255, 255, 255, 1)'}}>App Development</Link></p>
+            <p><Link to='/Services/UXUI' style={{textDecoration:'none',backgroundColor:'transparent',color:' rgba(255, 255, 255, 1)'}}>UX UI Design</Link></p>
+            <p>More...</p>
           </div>
           <div id='products'>
-          <div><h3>Products</h3></div>
-            <div><p>Coming Soon</p></div>
+          <h3>Products</h3>
+          <p>Coming Soon</p>
           </div>
           <div id='industries'>
-          <div><h3>Industries</h3></div>
-          <div><p><Link to='/Industries/Real-Estate' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Real Estate</Link></p></div>
-          <div><p><Link to='/Industries/IT' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>IT</Link></p></div>
-          <div><p><Link to='/Industries/Media' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Media</Link></p></div>
-          <div><p><Link to='/Industries/eCommerce' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>eCommerce</Link></p></div>
-           <div><p><Link to='/Industries/eGovernance' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>eGovernance</Link></p></div>
+          <h3>Industries</h3>
+          <p><Link to='/Industries/Real-Estate' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Real Estate</Link></p>
+          <p><Link to='/Industries/IT' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>IT</Link></p>
+          <p><Link to='/Industries/Media' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Media</Link></p>
+          <p><Link to='/Industries/eCommerce' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>eCommerce</Link></p>
+          <p><Link to='/Industries/eGovernance' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>eGovernance</Link></p>
           </div>
           <div id='company'>
-          <div><h3>Company</h3></div>
-          <div><p><Link to='/Careers' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Careers</Link></p></div>
-          <div><p>About</p></div>
-          <div><p>Contact</p></div>
-          <div><p><Link to='/privacy-Policy' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Privacy Policy</Link></p></div>
-          <div><p><Link to='/terms-Of-Use' style={{textDecoration:'none',backgroundColor:'rgba(11, 11, 11, 1)',color:'rgba(255, 255, 255, 1)'}}>Terms of Use</Link></p></div>
+          <h3>Company</h3>
+          <p><Link to='/Careers' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Careers</Link></p>
+          <p><Link to='/About' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>About</Link></p>
+          <p><Link to='/ContactUs' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Contact</Link></p>
+          <p><Link to='/privacy-Policy' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Privacy Policy</Link></p>
+          <p><Link to='/terms-Of-Use' style={{textDecoration:'none',backgroundColor:'transparent',color:'rgba(255, 255, 255, 1)'}}>Terms of Use</Link></p>
           </div>
         </div>
         </div>
         <div id='separatingline'></div>
-        <div id='copyright'>
-          <p>Sabrasta, Inc. 2024. All Rights Reserved</p>
-        </div>
+        <div id ='CopyRights'><p>Sabrasta, Inc. 2024. All Rights Reserved</p></div>
       </div>
     </div>
   )
